dec5c39f62bcb74745d32a98b30528ed1ade32b9
shamssaniat <shams.saniat@northsouth.edu>
2025-05-18 23:12:40 +0600

    structered the folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center">Rick and Morty <span className="text-primary">Wiki</span></h1>
+      <h1 className="text-center ubuntu my-4">Rick and Morty <span className="text-primary">Wiki</span></h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards />
+              <Cards />
+              <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
