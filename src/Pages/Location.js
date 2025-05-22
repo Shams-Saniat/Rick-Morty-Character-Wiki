0585e1d6b007dcbc0aed8243be7d972ff120ae20
shamssaniat <shams.saniat@northsouth.edu>
2025-05-23 02:34:21 +0600

    Added Location section

--- src/Pages/Location.js
+++ src/Pages/Location.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Cards from '../components/Cards/Cards'
+import InputGroup from '../Filters/Category/InputGroup';
 
-function Location () {
+const Episodes = () => {
+  let [id, setID] = useState(1);
+  let [info, setInfo] = useState([]);
+  let [results, setResults] = useState([]);
+  let { name, type, dimention } = info;
+  let api = `https://rickandmortyapi.com/api/location/${id}`;
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then(res => res.json(0));
+      setInfo(data);
+      console.log(data);
+
+      let a = await Promise.all(
+        data.residents.map((x) => {
+          return fetch(x).then((res) => res.json());
+        })
+      );
+      setResults(a);
+    })()
+  }, [api]);
   return (
-    <div>The locations are here</div>
+    <div className="container">
+      <div className="row">
+        <h1 className='text-center mb-4'>Location : {" "}
+          <span className='text-primary'>{name === "" ? "Unknown" : name}</span></h1>
+        <h5 className='text-center'>Dimention: {dimention === "" ? "Unknown" : dimention}</h5>
+        <h6 className='text-center'>Type: {type === "" ? "Unknown" : type}</h6>
+        <h5 className='text-center'>Dimention {dimention === "" ? "Unknown" : dimention}</h5>
+
+      </div>
+      <div className="row">
+        <div className="col-3">
+          <h4 className="text-center mb-4">Pick Location</h4>
+          <InputGroup setID={setID} name="Location" total={126} />
+        </div>
+        <div className="col-8">
+          <div className="row">
+            <Cards results={results} />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Location
+export default Episodes
